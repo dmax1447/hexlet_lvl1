5af3f1334ce2a098f06ff33f4df99d7ecc3cbb3f
dmax1447 <dmax1447@gmail.com>
2019-04-12 17:23:59 +0300

    реализовал brain-calc

--- src/index.js
+++ src/index.js
@@ -29,4 +29,42 @@ function playEvenGame() {
   console.log(`Congratulations, ${userName} ! `);
 }
 
-export { meetUser, playEvenGame };
+function playCalcGame() {
+  console.log('Welcome to Brain Games!');
+  console.log('What is the result of the expression?');
+  const userName = readlineSync.question('May i have your name? ');
+  console.log(`Hello, ${userName}`);
+
+  for (let i = 0; i < 3; i += 1) {
+    let questionAnswer;
+    const operationVariants = ['+', '-', '*'];
+    const arg1 = Math.round(Math.random() * 10);
+    const arg2 = Math.round(Math.random() * 10);
+    const operation = operationVariants[Math.round(Math.random() * 2)];
+    console.log(`Question: ${arg1} ${operation} ${arg2}`);
+    switch (operation) {
+      case '+':
+        questionAnswer = arg1 + arg2;
+        break;
+      case '-':
+        questionAnswer = arg1 - arg2;
+        break;
+      case '*':
+        questionAnswer = arg1 * arg2;
+        break;
+      default:
+        break;
+    }
+    const userAnswer = parseInt(readlineSync.question('Your Answer: '), 10);
+    if (questionAnswer === userAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${questionAnswer}.`);
+      console.log(`Lets try again, ${userName} ! `);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName} ! `);
+}
+
+export { meetUser, playEvenGame, playCalcGame };
