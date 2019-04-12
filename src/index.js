6371214b0ae8d8a0e58559b09ee3e89553673d89
dmax1447 <dmax1447@gmail.com>
2019-04-12 16:28:41 +0300

    правки по ревью even-game

--- src/index.js
+++ src/index.js
@@ -3,32 +3,30 @@ import readlineSync from 'readline-sync';
 function meetUser() {
   const userName = readlineSync.question('May i have your name? ');
   console.log(`Hello, ${userName}`);
-  return userName;
 }
+
 
 function playEvenGame() {
   console.log('Welcome to Brain Games!');
   console.log('Answer "yes" if number even otherwise answer "no"');
-  const userName = meetUser();
-  let isUserWinGame = true;
-  let stepsCount = 3;
-  while (isUserWinGame && stepsCount > 0) {
-    const randomNum = Math.round(Math.random() * 15);
-    const correctAnswer = (randomNum % 2) !== 0 ? 'yes' : 'no';
-    console.log(`Question: ${randomNum}`);
+  const userName = readlineSync.question('May i have your name? ');
+  console.log(`Hello, ${userName}`);
+  const isEven = num => (num % 2) !== 0;
+
+  for (let i = 0; i < 3; i += 1) {
+    const question = Math.round(Math.random() * 15);
+    const correctAnswer = isEven(question) ? 'yes' : 'no';
+    console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (correctAnswer === userAnswer) {
       console.log('correct!');
     } else {
       console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
       console.log(`Lets try again, ${userName} ! `);
-      isUserWinGame = false;
+      return;
     }
-    stepsCount -= 1;
   }
-  if (isUserWinGame) {
-    console.log(`Congratulations, ${userName} ! `);
-  }
+  console.log(`Congratulations, ${userName} ! `);
 }
 
 export { meetUser, playEvenGame };
